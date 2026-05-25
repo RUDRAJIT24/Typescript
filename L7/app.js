4f0179fb070821c6e44fddb12ae6934b1f973a79
Rudrajit Saha Roy <rudrajitsaharoy24@gmail.com>
2026-05-25 10:53:29 +0530

    Type guards, Type casting/Type assertion

---
+++ L7/app.js
@@ -0,0 +1,19 @@
+"use strict";
+//Type Guards, Type Assertion
+//Type Guards
+// type Combinable = string | number;
+// type Numeric = number | boolean;
+// type Universal = Combinable & Numeric
+// function add(a: Combinable, b: Combinable) {
+//     //add type guards
+//     if(typeof a === "string" || typeof b === "string") {
+//         return a.toString() + b.toString();
+//     }
+//     return a + b
+// }
+// console.log(add(20, 30));
+// console.log(add("20", 30));
+//  Type Assertion / Type Casting
+// const userInput = <HTMLInputElement>document.getElementById("user-input")!
+const userInput = document.getElementById("user-input");
+userInput.value = "Hello";
